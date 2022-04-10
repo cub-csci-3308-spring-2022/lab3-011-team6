b29ba2807ea992484682d7ffbc617e2bb90473c2
Anna Rahn <anra8571@colorado.edu>
2022-04-10 11:10:53 -0600

    setting up server.spec.js file

---
+++ code-stuffs/server.spec.js
@@ -0,0 +1,10 @@
+const server = require("server");
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+chai.should();
+chai.use(chaiHttp);
+const{assert, expect} = chai;
+
+describe("Server!", () => {
+    
+})
