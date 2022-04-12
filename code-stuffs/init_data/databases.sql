6f7ad6ba4dc137daf3f2fe0b8d0239253cf8b346
Anna Rahn <anra8571@colorado.edu>
2022-04-12 15:20:23 -0600

    trying to upload from csv to books_db

--- code-stuffs/init_data/databases.sql
+++ code-stuffs/init_data/databases.sql
@@ -6,20 +6,20 @@ CREATE TABLE IF NOT EXISTS users_db
     genres INT[]
 );
 
-CREATE TABLE IF NOT EXISTS books_db (
-    title VARCHAR(30) PRIMARY KEY,
-    category VARCHAR(30)
-);
-
-INSERT INTO books_db(title, category)
-VALUES ('Inward Journey', 'Medical'),
-('The Boston Directory', 'Boston')
-; 
-
 -- CREATE TABLE IF NOT EXISTS books_db (
 --     title VARCHAR(30) PRIMARY KEY,
---     category VARCHAR (30)
+--     category VARCHAR(30)
 -- );
+
+-- INSERT INTO books_db(title, category)
+-- VALUES ('Inward Journey', 'Medical'),
+-- ('The Boston Directory', 'Boston')
+-- ; 
+
+CREATE TABLE IF NOT EXISTS books_db (
+    title VARCHAR(30) PRIMARY KEY,
+    categories VARCHAR (30)
+);
 -- COPY books_db
 -- FROM 'google_books_dataset.csv'
 -- DELIMITER ',';
